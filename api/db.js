f76f8367c34aa89b1aa27b8426f91f13b872df4f
John-Patrick-Russel-Lalo <johnpatricklalo@gmail.com>
2025-03-09 22:05:59 +0800

    Update db.js

--- api/db.js
+++ api/db.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
     const { Pool } = pg;
 
     const pool = new Pool({
-        connectionString: "postgresql://TPDB_owner:npg_hEIU5ZTVQi8A@ep-yellow-night-a8e2cfte-pooler.eastus2.azure.neon.tech/TPDB?sslmode=require"
+        connectionString: process.env.DATABASE_URL
     });
 
     const result = await pool.query("SELECT * FROM projects");
