1ddf6595f6b111d3a8f1810513856716fec4e765
John-Patrick-Russel-Lalo <johnpatricklalo@gmail.com>
2025-03-10 10:54:38 +0800

    Update saveUser.js

--- api/saveUser.js
+++ api/saveUser.js
@@ -4,6 +4,8 @@ const pool = new Pool({
     connectionString: process.env.POSTGRES_URL,
 });
 
+console.log(pool);
+
 export default async function handler(req, res) {
     if (req.method !== "POST") {
         return res.status(405).json({ error: "Method not allowed" });
