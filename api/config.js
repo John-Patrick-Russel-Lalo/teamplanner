6213e1510d31f382597ac061653ad2484fb1af8b
John-Patrick-Russel-Lalo <johnpatricklalo@gmail.com>
2025-03-10 10:40:16 +0800

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,3 +1,3 @@
 export default function handler(req, res) {
-    res.status(200).json({ clientId: "YOUR_GOOGLE_CLIENT_ID" });
+    res.status(200).json({ clientId: "597591645648-c6ainfa35peqmcqt8a9g1otr74vn3mgn.apps.googleusercontent.com" });
 }
