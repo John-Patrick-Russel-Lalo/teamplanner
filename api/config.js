07c6e646085df6c9646497d97928a77a38877633
John-Patrick-Russel-Lalo <johnpatricklalo@gmail.com>
2025-03-11 14:39:40 +0800

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,3 +1,15 @@
 export default function handler(req, res) {
     res.status(200).json({ clientId: "597591645648-c6ainfa35peqmcqt8a9g1otr74vn3mgn.apps.googleusercontent.com" });
 }
+
+import { Pool } from 'pg';
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+export default pool;
+
