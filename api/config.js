1a7f990cfff46ea56c933d21979eb49d41cff717
John-Patrick-Russel-Lalo <johnpatricklalo@gmail.com>
2025-03-11 15:19:24 +0800

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,6 +1,4 @@
-export default function handler(req, res) {
-    res.status(200).json({ clientId: "597591645648-c6ainfa35peqmcqt8a9g1otr74vn3mgn.apps.googleusercontent.com" });
-}
+
 
 import { Pool } from 'pg';
 
